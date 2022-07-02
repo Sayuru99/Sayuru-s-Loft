6a051078ca98f3b7080077a3b00bc1786b94e392
Sayuru99 <saurudealwis@ymail.com>
2022-07-02 20:09:15 +0530

    edit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from django.urls import path
+from django.urls import path, url
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    url(r'^about/$', views.about, name='about'),
 ]
