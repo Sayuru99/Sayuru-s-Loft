ef976fc2e888c3428f10f6a2065d31dfb8b211be
Sayuru99 <saurudealwis@ymail.com>
2022-07-02 20:18:49 +0530

    edit

--- blog/models.py
+++ blog/models.py
@@ -35,3 +35,19 @@ class Comment(models.Model):
 
     def __str__(self):
         return 'Comment {} by {}'.format(self.body, self.name)
+
+
+class about(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from .models import Post
+from .models import Post, about
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -8,3 +8,7 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class about(generic.DetailView):
+    model = about
+    template_name = 'about.html'
