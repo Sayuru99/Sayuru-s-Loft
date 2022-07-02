ea0dcb53964ffb50984d7fd36153542ffac38775
Sayuru99 <saurudealwis@ymail.com>
2022-07-02 21:36:38 +0530

    edit

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,7 @@
 from . import views
-from django.urls import path, url
-from blog.views import about
+from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    url(r'^/about/$', about.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.views import generic
 from .models import Post
-from django.views.generic import View
-from django.shortcuts import render
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -11,7 +10,3 @@ class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class about(View):
-
-    def get(self, request):
-        return render(request, "about.html")
